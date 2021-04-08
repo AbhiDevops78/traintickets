a49ba3d592fcbce89679c387e1657935a33a8f87
abhi <abhi@xyz.com>
2021-04-08 16:46:28 +0530

    code for train tickets

---
+++ src/main/java/traintickets/RoutesImplementation.java
@@ -0,0 +1,22 @@
+package traintickets;
+
+import com.datasonnet.document.MediaTypes;
+import org.apache.camel.language.datasonnet.DatasonnetExpression;
+import org.springframework.stereotype.Component;
+
+/**
+ * The RoutesImplementation class holds implementations for the end points configured in RoutesGenerated.
+ * These routes are based on operation Ids, that correspond to an API end point:  method+path.
+ *
+ * @author Maven Archetype (camel-openapi-archetype)
+ */
+@Component
+public class RoutesImplementation extends BaseRestRouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        super.configure();
+
+        // Implementation routes
+    }
+}
